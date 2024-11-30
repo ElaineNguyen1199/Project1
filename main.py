26638110054032778dd12fb0937781fc897b20c3
ElaineNguyen1199 <elainenguyen@unomaha.edu>
2024-11-30 14:49:30 -0600

    new main.py by taking out the logic and putting it into a different file and now main.py contains creating the widget

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-class Account:
-    def __init__(self, name, balance=0):
-        self.__account_name = name
-        self.__account_balance = 0
-        self.set_balance(balance)
+from gui import *
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            return True
-        else:
-            return False
+def main():
+    window = Tk()
+    window.title("ATM")
+    window.geometry('300x400')
+    window.resizable(False, False)
+    AccountGUI(window)
+    window.mainloop()
 
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return True
-        else:
-            return False
-
-    def get_balance(self):
-        return self.__account_balance
-
-    def get_name(self):
-        return self.__account_name
-
-    def set_balance(self, value):
-        if value < 0:
-            self.__account_balance = 0
-        else:
-            self.__account_balance = value
-
-    def set_name(self, value):
-        self.__account_name = value
-
-    def __str__(self):
-        return f'Account name = {self.__account_name}, Account balance = {self.__account_balance:.2f}'
+if __name__ == '__main__':
+    main()
